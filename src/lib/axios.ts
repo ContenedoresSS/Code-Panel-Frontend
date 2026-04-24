c7020f5ea1df42564b31408f09d744a8b6b519ea
Carlos Coronado <ccoronado@ekasoft.com>
2026-04-24 13:53:23 -0600

    chore: update api backend url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1', 
+    baseURL: 'https://codepanel.orchfr.duckdns.org/api/v1', 
     headers: {
         'Content-Type': 'application/json',
     },
